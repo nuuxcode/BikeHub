bd669a67bfe9f30e5a3be29482916424a85367e4
amasin76 <amasin76@gmail.com>
2023-11-20 07:16:26 +0100

    Add: `Park` module

---
+++ api/src/modules/park/park.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { Park, Prisma } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ParkService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(
+    parkWhereUniqueInput: Prisma.ParkWhereUniqueInput,
+  ): Promise<Park | null> {
+    return this.prisma.park.findUnique({
+      where: parkWhereUniqueInput,
+    });
+  }
+
+  async findAll(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ParkWhereUniqueInput;
+    where?: Prisma.ParkWhereInput;
+    orderBy?: Prisma.ParkOrderByWithRelationInput;
+  }): Promise<Park[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.park.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async create(data: Prisma.ParkCreateInput): Promise<Park> {
+    return this.prisma.park.create({
+      data,
+    });
+  }
+
+  async update(params: {
+    where: Prisma.ParkWhereUniqueInput;
+    data: Prisma.ParkUpdateInput;
+  }): Promise<Park> {
+    const { data, where } = params;
+    return this.prisma.park.update({
+      data,
+      where,
+    });
+  }
+
+  async delete(where: Prisma.ParkWhereUniqueInput): Promise<Park> {
+    return this.prisma.park.delete({
+      where,
+    });
+  }
+}
