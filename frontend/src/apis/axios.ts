d7bdd48e20c75ea30f05a7d85e50581afb952fd6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-03 22:01:11 +0100

    Add env to control API MODE

--- frontend/src/apis/axios.ts
+++ frontend/src/apis/axios.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+let BACKEND_URL;
+if (import.meta.env.VITE_MODE === 'prod') {
+  BACKEND_URL = import.meta.env.VITE_BACK_END_PROD;
+}
+if (import.meta.env.VITE_MODE === 'dev') {
+  BACKEND_URL = import.meta.env.VITE_BACK_END_DEV;
+}
+if (import.meta.env.VITE_MODE === 'local') {
+  BACKEND_URL = import.meta.env.VITE_BACK_END_LOCAL;
+}
 export default axios.create({
-  baseURL: "http://localhost:3300/api/v1/",
+  baseURL: BACKEND_URL,
 });
