333405a162e721a1a47e00c9e5d1d28d4ffb0a3f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-04 04:54:13 +0100

    add checkAuthentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/header/header.component";
 import { Toaster } from "react-hot-toast";
 import { Routes } from "./routes/routes";
 import "./index.css";
+
 function App() {
   return (
     <div className="h-screen">
