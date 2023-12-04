a273d30cebc96957ed7e036df8393659fdb39c2d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-04 10:20:45 +0100

    Change profile to UserSettings

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -3,7 +3,7 @@ import { Routes as ReactRouterRoutes, Route } from "react-router-dom";
 import LoginPage from "../pages/auth/login.page";
 import SignupPage from "../pages/auth/signup.page";
 import HomePage from "../pages/home.page";
-import Profile from "../pages/profile/profle.page";
+import UserSettings from "../pages/profile/UserSettings";
 import NotFoundPage from "../pages/error/notFound.page";
 import PrivateRoutes from "../components/route/privateRoutes.component";
 
@@ -11,7 +11,7 @@ const Routes: React.FC = () => {
   return (
     <ReactRouterRoutes>
       <Route element={<PrivateRoutes />}>
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/UserSettings" element={<UserSettings />} />
       </Route>
       <Route path="/" element={<HomePage />} />
       <Route path="login" element={<LoginPage />} />
