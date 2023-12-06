962ba9955bc354ccaddab5df5d877c0180ddfeec
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-06 04:36:09 +0100

    adding usersettings page still on progress

--- frontend/src/pages/profile/UserSettings.tsx
+++ frontend/src/pages/profile/UserSettings.tsx
@@ -9,43 +9,39 @@ import {
   Stack,
   Text,
   useColorModeValue,
+  VStack,
 } from "@chakra-ui/react";
 import axios from "../../apis/axios";
 
 const UserSettings: React.FC = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
+  const [error2, setError2] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
-  const handleFormSubmit = async (e: React.FormEvent) => {
+  const handleNameEmailSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // localstorage user : {"id":30,"name":"Mounssif BOUHLAOUI","email":"cv.bouhlaoui@gmail.com","accessToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzAsIm5hbWUiOiJNb3Vuc3NpZiBCT1VITEFPVUkiLCJlbWFpbCI6ImN2LmJvdWhsYW91aUBnbWFpbC5jb20iLCJwYXNzd29yZCI6bnVsbCwicm9sZSI6InVzZXIiLCJjcmVhdGVkX2F0IjoiMjAyMy0xMi0wNFQwNjo1MDo1OS45NjJaIiwidXBkYXRlZF9hdCI6IjIwMjMtMTItMDRUMDY6NTA6NTkuOTYyWiIsImlhdCI6MTcwMTY3MjY1OSwiZXhwIjoxNzAxNzU5MDU5fQ.fJaTXhVMgv9spUVpd1s_0J55j1vUbJvoWxgi2gmNhhI"}
-    // get current user id from localstorage
     const userId = JSON.parse(localStorage.getItem("user") || "{}").id;
-    console.log("userId", userId);
+
     // Basic form validation
     if (!name || !email) {
       setError("Name and email are required fields");
       return;
     }
 
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-      return;
-    }
-
-    // Send updated user settings to the server
-    // http://localhost:3300/api/v1/users/user/30
+    // Send updated name and email to the server
     try {
-      const response = await axios.post(`/api/v1/users/user/${userId}`, {
+      const response = await axios.put(`/users/user/${userId}`, {
         name,
         email,
-        password,
+      }, {
+        withCredentials: true,
       });
-      console.log("response", response);
+
       setSuccessMessage(response.data.message);
       setError("");
     } catch (error) {
@@ -54,22 +50,50 @@ const UserSettings: React.FC = () => {
     }
   };
 
+  const handlePasswordSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const userId = JSON.parse(localStorage.getItem("user") || "{}").id;
+
+    // Basic form validation
+    if (password !== confirmPassword) {
+      setError2("Passwords do not match");
+      return;
+    }
+
+    // Send updated password to the server
+    try {
+      const response = await axios.put(`/users/user/${userId}`, {
+        oldpassword : oldPassword,
+        newpassword: password,
+      }, {
+        withCredentials: true,
+      });
+
+      setSuccessMessage(response.data.message);
+      setError2("");
+    } catch (error) {
+      setSuccessMessage("");
+      setError2("Error updating user settings");
+    }
+  };
+
   return (
     <Stack
       spacing={4}
-      maxWidth="400px"
+      maxWidth="800px"
       mx="auto"
       mt={8}
       p={6}
       rounded="md"
       shadow="md"
       bg={useColorModeValue("white", "gray.800")}
+      direction={{ base: "column", sm: "row" }}
     >
-      <Text fontSize="xl" fontWeight="bold" mb={4}>
-        User Settings
-      </Text>
-      <form onSubmit={handleFormSubmit}>
-        {/* User settings form inputs */}
+      {/* Name and Email Form */}
+      <VStack as="form" onSubmit={handleNameEmailSubmit} spacing={4} flex={1}>
+        <Text fontSize="xl" fontWeight="bold" mb={4}>
+          Update Name and Email
+        </Text>
         <FormControl isInvalid={!!error}>
           <FormLabel htmlFor="name">Name</FormLabel>
           <Input
@@ -81,7 +105,7 @@ const UserSettings: React.FC = () => {
           <FormErrorMessage>{error}</FormErrorMessage>
         </FormControl>
 
-        <FormControl mt={4} isInvalid={!!error}>
+        <FormControl isInvalid={!!error}>
           <FormLabel htmlFor="email">Email</FormLabel>
           <Input
             type="email"
@@ -92,17 +116,45 @@ const UserSettings: React.FC = () => {
           <FormErrorMessage>{error}</FormErrorMessage>
         </FormControl>
 
-        <FormControl mt={4}>
-          <FormLabel htmlFor="password">Password</FormLabel>
+        <Button
+          type="submit"
+          colorScheme="teal"
+          mt={6}
+          width="100%"
+          isLoading={false}
+        >
+          Save Name and Email
+        </Button>
+      </VStack>
+
+      {/* Change Password Form */}
+      <VStack as="form" onSubmit={handlePasswordSubmit} spacing={4} flex={1}>
+        <Text fontSize="xl" fontWeight="bold" mb={4}>
+          Change Password
+        </Text>
+        <FormControl isInvalid={!!error2}>
+          <FormLabel htmlFor="oldPassword">Old Password</FormLabel>
+          <Input
+            type="password"
+            id="oldPassword"
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
+          />
+          <FormErrorMessage>{error2}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!error2}>
+          <FormLabel htmlFor="password">New Password</FormLabel>
           <Input
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <FormErrorMessage>{error2}</FormErrorMessage>
         </FormControl>
 
-        <FormControl mt={4}>
+        <FormControl isInvalid={!!error2}>
           <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
           <Input
             type="password"
@@ -110,6 +162,7 @@ const UserSettings: React.FC = () => {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
+          <FormErrorMessage>{error2}</FormErrorMessage>
         </FormControl>
 
         <Button
@@ -119,9 +172,9 @@ const UserSettings: React.FC = () => {
           width="100%"
           isLoading={false}
         >
-          Save Changes
+          Change Password
         </Button>
-      </form>
+      </VStack>
 
       {successMessage && (
         <Box mt={4} color="green.500" fontWeight="bold">
