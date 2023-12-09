c793d21201a87a4c998ee785f9302f4b8739fee8
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-09 23:08:17 +0100

    update seed script

--- api/prisma/seeds/users.seed.ts
+++ api/prisma/seeds/users.seed.ts
@@ -4,8 +4,7 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 export async function seedUsers() {
-  const users = faker.helpers.multiple(createRandomUser, { count: 20 });
-  const createdUsers = [];
+  const users = faker.helpers.multiple(createRandomUser, { count: 5 });  const createdUsers = [];
 
   for (const user of users) {
     const createdUser = await prisma.user.create({
