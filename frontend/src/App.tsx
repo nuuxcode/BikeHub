245a7453bbbc9e67c49cb2f7d04d28b421f65037
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-10 00:19:08 +0100

    Add go to top button, fix links and refresh scroll

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,31 @@ import Header from "./components/header/header.component";
 import { Toaster } from "react-hot-toast";
 import { Routes } from "./routes/routes";
 import "./index.css";
+import { useEffect } from 'react';
 import Footer from "./components/footer/footer.component";
+import GoToTopButton from "./components/GoToTopButton";
 
 function App() {
+  function ScrollToSection() {
+    useEffect(() => {
+      const hash = window.location.hash;
+      if (hash) {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    }, []);
+    return null;
+  }
   return (
     <div className="h-screen">
+      <ScrollToSection />
       <Header />
       <Routes />
       <Footer />
       <Toaster />
+      <GoToTopButton />
     </div>
   );
 }
