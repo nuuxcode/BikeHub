01e0926ae51a99f4f39d06439e476c8e0c99c539
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-11 11:33:02 +0100

    Add delete account

--- frontend/src/pages/settingProfile/partials/deleteUserForm.component.tsx
+++ frontend/src/pages/settingProfile/partials/deleteUserForm.component.tsx
@@ -1,108 +1,108 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
-  Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
   InputRightElement,
-  useDisclosure,
+  Button,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "../../../apis/axios";
+import { useAuth } from "../../../hooks/useAuth";
 
-export default function DeleteUserForm({
-  className = "",
-}: {
+interface DeleteUserFormProps {
   className?: string;
-}) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const cancelRef = React.useRef<HTMLButtonElement>(null);
+}
+
+const DeleteUserForm: React.FC<DeleteUserFormProps> = ({
+  className = "",
+}) => {
+  const { user, logout } = useAuth();
   const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState("");
-  const [errPassword, setErrPassword] = useState(false);
-  const [errMsg, setErrMsg] = useState("");
+  const [errPassword, setErrPassword] = useState("");
+  const toast = useToast({ position: "top" });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrPassword("");
+
+    try {
+      const response = await axios.delete(`/users/delete/${user?.id}`, { data: { password }, withCredentials: true, });
+      // Handle success response
+      console.log(response);
+      toast({
+        title: "Account deleted",
+        description: "Your account has been deleted.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      logout();
+    } catch (error: any) {
+      console.log(error);
+      let errorMessage = error?.response?.data?.message;
+      if (typeof errorMessage === "string") {
+        errorMessage = error?.response?.data?.message;
+      } else {
+        errorMessage = error?.response?.data?.message.join(", ");
+      }
+      toast({
+        title: "Error",
+        description: errorMessage || "Something went wrong.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <section className={`p-5 space-y-6 ${className}`}>
       <header>
         <h2 className="text-lg font-medium text-gray-900">Delete Account</h2>
-
         <p className="mt-1 text-sm text-gray-600">
-          Once your account is deleted, all of its resources and data will be
-          permanently deleted. Before deleting your account, please download any
-          data or information that you wish to retain.
+          Once your account is deleted, all of its resources and data will be permanently deleted.
+          Before deleting your account, please download any data or information that you wish to retain.
         </p>
       </header>
 
-      <Button colorScheme="red" onClick={onOpen}>
-        Delete Account
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <FormControl id="password" isInvalid={errPassword !== ""}>
+          <FormLabel>Password</FormLabel>
+          <InputGroup>
+            <Input
+              isInvalid={errPassword !== ""}
+              errorBorderColor="crimson"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <InputRightElement h={"full"}>
+              <Button
+                variant={"ghost"}
+                onClick={() =>
+                  setShowPassword((showPassword) => !showPassword)
+                }
+              >
+                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {errPassword && (
+            <FormErrorMessage>{errPassword}</FormErrorMessage>
+          )}
+        </FormControl>
 
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <form>
-              <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                Are you sure? You can't undo this action afterwards.
-              </AlertDialogHeader>
-
-              <AlertDialogBody>
-                Once your account is deleted, all of its resources and data will
-                be permanently deleted. Please enter your password to confirm
-                you would like to permanently delete your account.
-              </AlertDialogBody>
-              <AlertDialogBody>
-                <FormControl id="password" isInvalid={errPassword}>
-                  <FormLabel>Password</FormLabel>
-                  <InputGroup>
-                    <Input
-                      isInvalid={errMsg != ""}
-                      errorBorderColor="crimson"
-                      type={showPassword ? "text" : "password"}
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-
-                    <InputRightElement h={"full"}>
-                      <Button
-                        variant={"ghost"}
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
-                      >
-                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
-                  <FormErrorMessage>
-                    password should not be empty
-                  </FormErrorMessage>
-                </FormControl>
-              </AlertDialogBody>
-
-              <AlertDialogFooter>
-                <Button ref={cancelRef} onClick={onClose}>
-                  Cancel
-                </Button>
-                <Button colorScheme="red" onClick={onClose} ml={3}>
-                  Delete
-                </Button>
-              </AlertDialogFooter>
-            </form>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+        <Button type="submit" colorScheme="red" mt={4}>
+          Delete Account
+        </Button>
+      </form>
     </section>
   );
-}
+};
+
+export default DeleteUserForm;
