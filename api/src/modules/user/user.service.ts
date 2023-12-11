d9c122c18e4bfc5ece51454b178c0b287602ce6c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-11 08:31:22 +0100

    updateuser add the password change

--- api/src/modules/user/user.service.ts
+++ api/src/modules/user/user.service.ts
@@ -2,10 +2,11 @@ import { Prisma, User } from '@prisma/client';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthHelpers } from '../../shared/helpers/auth.helpers';
 import { PrismaService } from '../prisma/prisma.service';
+import { UpdateUser } from './../auth/auth.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findUser(
     userWhereUniqueInput: Prisma.UserWhereUniqueInput,
@@ -40,17 +41,36 @@ export class UserService {
 
   async updateUser(params: {
     where: Prisma.UserWhereUniqueInput;
-    data: Prisma.UserUpdateInput;
+    data: UpdateUser;
   }): Promise<User> {
-    const { where, data } = params;
+    let { where, data } = params;
+    console.log("data recieved:", data)
     const user = await this.prisma.user.findUnique({
       where: where,
     });
-    data.password = user.password;
+    console.log("user found:", user)
+    //if request has newPassword, mean we gonna update password
+    if (data.hasOwnProperty('newPassword')) {
+      const isMatch = await AuthHelpers.verify(
+        data.oldPassword,
+        user.password,
+      );
+      if (!isMatch) {
+        console.log("old password doesn't match")
+        throw new UnauthorizedException("Old password doesn't match");
+      }
+      data.password = data.newPassword;
+      delete data.oldPassword;
+      delete data.newPassword;
+    } else {
+      data.password = user.password;
+    }
+    console.log("data updated:", data)
     const updatedUser = await this.prisma.user.update({
       data,
       where,
     });
+    console.log("user updated:", updatedUser)
     delete updatedUser.password;
     return updatedUser;
   }
