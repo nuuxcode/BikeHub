65577bf07a187aa0d3627948a4336fbd0a20ff8d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-12-16 08:08:50 +0100

    Slider: Update text, tweak animation and opacity

--- frontend/src/components/home/landingSwiper/cardSlider.component.tsx
+++ frontend/src/components/home/landingSwiper/cardSlider.component.tsx
@@ -39,6 +39,7 @@ const CardSlider = ({
         w={"full"}
         h={"full"}
         position={"absolute"}
+        style={{ opacity: 0.5 }}
       ></Box>
       {/* <Box
           bgImage={img}
@@ -50,6 +51,19 @@ const CardSlider = ({
         ></Box> */}
       <motion.p
         className="text-teal-500 sm:text-lg text-sm font-bold  px-2 z-10 mb-4"
+        style={{ textShadow: '2px 2px 2px rgba(0, 0, 0, 0.4)' }}
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 1, delay: 0.2 }}
+      >
+        {sousTitle}
+      </motion.p>
+      <motion.p
+        className="text-gray-50 sm:text-5xl text-3xl font-bold z-10"
         variants={{
           hidden: { opacity: 0, y: 75 },
           visible: { opacity: 1, y: 0 },
@@ -58,20 +72,9 @@ const CardSlider = ({
         animate="visible"
         transition={{ duration: 1, delay: 0.5 }}
       >
-        {sousTitle}
-      </motion.p>
-      <motion.p
-        className="text-gray-50 sm:text-6xl text-3xl font-bold z-10"
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 },
-        }}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 1, delay: 1.5 }}
-      >
         {title}
       </motion.p>
+      {/* green line */}
       <motion.div
         className="w-16 h-1 rounded-xl bg-teal-100 z-10"
         variants={{
@@ -80,34 +83,34 @@ const CardSlider = ({
         }}
         initial="hidden"
         animate="visible"
-        transition={{ duration: 1, delay: 2.5 }}
+        transition={{ duration: 1, delay: 1 }}
       />
-      <Box>
-        <motion.p
-          className="text-gray-50 sm:text-lg text-sm font-semibold z-10"
-          variants={{
-            hidden: { opacity: 0, x: 150 },
-            visible: { opacity: 1, x: 0 },
-          }}
-          initial="hidden"
-          animate="visible"
-          transition={{ duration: 1, delay: 3.5 }}
-        >
-          {text1}
-        </motion.p>
-        <motion.p
-          className="text-gray-50 sm:text-lg text-sm font-semibold z-10"
-          variants={{
-            hidden: { opacity: 0, x: -150 },
-            visible: { opacity: 1, x: 0 },
-          }}
-          initial="hidden"
-          animate="visible"
-          transition={{ duration: 1, delay: 4 }}
-        >
-          {text2}
-        </motion.p>
-      </Box>
+      <motion.p
+        className="text-gray-50 sm:text-lg text-sm font-semibold z-10"
+        style={{ zIndex: 20 }}
+        variants={{
+          hidden: { opacity: 0, x: 150 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 2.5, delay: 1.5 }}
+      >
+        {text1}
+      </motion.p>
+      <motion.p
+        className="text-gray-50 sm:text-lg text-sm font-semibold z-10"
+        style={{ zIndex: 20 }}
+        variants={{
+          hidden: { opacity: 0, x: -150 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 2.5, delay: 1.5 }}
+      >
+        {text2}
+      </motion.p>
     </Flex>
   );
 };
